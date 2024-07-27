d28d0128bb9c92ad07662297c6d8a3c727a8531b
dilaprastiwi5948 <terim1148@gmail.com>
2024-07-27 14:52:47 +0700

    tes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
 import MainLayout from '../components/layouts/MainLayout'
+import { FaBell } from "react-icons/fa6";
 
 export default function Home() {
   return (
     <MainLayout>
-        <h1>KONTOL</h1>
+        <div className="div">
+          <h1>kotak</h1>
+          <FaBell/>
+        </div>
     </MainLayout>
   )
 }
