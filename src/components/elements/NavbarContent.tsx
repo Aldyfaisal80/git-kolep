a79eb15762286d06c43db51cc125b65496efa999
Aldyfaisal80 <98567736+Aldyfaisal80@users.noreply.github.com>
2024-07-28 14:23:43 +0700

    navbar component done

---
+++ src/components/elements/NavbarContent.tsx
@@ -0,0 +1,58 @@
+const NavbarLayouts = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className="flex items-center">
+            {children}
+        </div>
+    )
+}
+
+const NavbarSelect = ({ options, icon }: { options: { value: string, description: string }[], icon: React.ReactNode }) => {
+    return (
+        <div className="w-full flex p-2 bg-slate-100 rounded-md">
+            <select className="p-2 outline-none">
+                {options.map((option, index) => (
+                    <option key={index} value={option.value}>
+                        {option.description}
+                    </option>
+                ))}
+            </select>
+            <input className="p-2 flex justify-center outline-none w-full bg-slate-100" type="text" placeholder="Search..." />
+            <button className="">
+                {icon}
+            </button>
+        </div>
+    );
+}
+
+const IconsGroup = ({ icons }: { icons: React.ReactNode[] }) => {
+    return (
+        <div className="flex items-center justify-around w-3/4">
+            {icons.map((icon, index) => (
+                <div key={index}>
+                    {icon}
+                </div>
+            ))}
+        </div>
+    );
+};
+
+const ProfileUsers = ({ image}: { image: string, }) => {
+    return (
+        <div className="flex border-l-2 gap-4 border-solid items-center relative">
+            <img src={image} className="rounded-full ml-6 w-[40px]" alt="" />
+            <select name="profile" id="profile">
+                <option className="" value="profile"></option>
+            </select>
+        </div>
+    )
+}
+
+
+NavbarLayouts.NavbarSelect = NavbarSelect
+NavbarLayouts.IconsGroup = IconsGroup
+NavbarLayouts.ProfileUsers = ProfileUsers
+
+
+export default NavbarLayouts
+
+
